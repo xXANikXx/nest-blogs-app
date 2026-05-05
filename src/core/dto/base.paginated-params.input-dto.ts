54395029b994e3d882a26dc81acdef6d14e4973f
ANik <annikita.and@gmail.com>
2026-05-05 23:43:18 +0500

    Errors fixed

--- src/core/dto/base.paginated-params.input-dto.ts
+++ src/core/dto/base.paginated-params.input-dto.ts
@@ -1,21 +1,29 @@
 import { Type } from 'class-transformer';
+import { IsEnum, IsNumber, IsOptional } from 'class-validator';
+
+export enum SortDirection {
+  Asc = 'asc',
+  Desc = 'desc',
+}
 
 //базовый класс для query параметров с пагинацией
 //значения по-умолчанию применятся автоматически при настройке глобального ValidationPipe в main.ts
 export class BaseQueryParams {
-  //для трансформации в number
+  @IsNumber()
+  @IsOptional()
   @Type(() => Number)
   pageNumber: number = 1;
+
+  @IsNumber()
+  @IsOptional()
   @Type(() => Number)
   pageSize: number = 10;
+
+  @IsEnum(SortDirection)
+  @IsOptional()
   sortDirection: SortDirection = SortDirection.Desc;
 
   calculateSkip() {
     return (this.pageNumber - 1) * this.pageSize;
   }
-}
-
-export enum SortDirection {
-  Asc = 'asc',
-  Desc = 'desc',
 }
--- src/modules/user_accounts/api/input-dto/get-users-query-params.input-dto.ts
+++ src/modules/user_accounts/api/input-dto/get-users-query-params.input-dto.ts
@@ -1,8 +1,17 @@
 import { BaseQueryParams } from '../../../../core/dto/base.paginated-params.input-dto';
 import { UsersSortBy } from './users-sort-by';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class GetUsersQueryParams extends BaseQueryParams {
-  sortBy = UsersSortBy.CreatedAt;
+  @IsEnum(UsersSortBy)
+  @IsOptional()
+  sortBy: UsersSortBy = UsersSortBy.CreatedAt;
+
+  @IsString()
+  @IsOptional()
   searchLoginTerm: string | null = null;
+
+  @IsString()
+  @IsOptional()
   searchEmailTerm: string | null = null;
 }
